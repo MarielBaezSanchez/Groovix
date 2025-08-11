12f41fe273d8443db993b1c94c6902c0e5541f3c
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-08-10 19:56:41 -0600

    Obtener eventos en home

---
+++ client/src/pages/private/home/common/event-card.tsx
@@ -0,0 +1,11 @@
+import type { EventType } from "../../../../interfaces"
+
+function EventCard({event} : {
+    event: EventType;
+}) {
+  return (
+    <div>{event.name}</div>
+  )
+}
+
+export default EventCard
--- client/src/pages/private/home/index.tsx
+++ client/src/pages/private/home/index.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
 import usersGlobalStore, { type UsersStoreType } from "../../../store/users-store";
+import { message } from "antd";
+import { getEvents } from "../../../api-services/events-service";
+import type { EventType } from "../../../interfaces";
+import EventCard from "./common/event-card";
 
 
 function Homepage() {
-  const {currentUser} = usersGlobalStore() as UsersStoreType;
+  const [events, setEvents] = useState<EventType[]>([]);
+  const [loading, setLoading] = useState(false);
+  const { currentUser } = usersGlobalStore() as UsersStoreType;
+
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+      const response = await getEvents();
+      setEvents(response.data);
+    } catch (error) {
+      message.error("Falla al obtener los eventos");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
   return (
     <div>
-      <h1>HomePage</h1>
       <p>Welcome, {currentUser?.name}!</p>
+
+      <div className="flex flex-col gap-">
+        {events.map((event: any) => (
+          <EventCard key={event._id} event={event} />
+        ))}
+      </div>
     </div>
   )
 }
