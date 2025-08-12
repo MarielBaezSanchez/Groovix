f16e4a0c6330c356e9373953eefb74a5a202903f
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-08-12 00:40:10 -0600

    Filtros en UI

--- client/src/pages/private/home/index.tsx
+++ client/src/pages/private/home/index.tsx
@@ -4,10 +4,15 @@ import { message } from "antd";
 import { getEvents } from "../../../api-services/events-service";
 import type { EventType } from "../../../interfaces";
 import EventCard from "./common/event-card";
+import Filters from "./common/filters";
 
 
 function Homepage() {
   const [events, setEvents] = useState<EventType[]>([]);
+  const [filters, setFilters] = useState({
+    searchText:"",
+    date: "",
+  });
   const [loading, setLoading] = useState(false);
   const { currentUser } = usersGlobalStore() as UsersStoreType;
 
@@ -31,8 +36,9 @@ function Homepage() {
   return (
     <div>
       <p>Welcome, {currentUser?.name}!</p>
+      <Filters filters={filters} setFilters={setFilters} onFilter={getData}/>
 
-      <div className="flex flex-col gap-">
+      <div className="flex flex-col gap-7 mt-7">
         {events.map((event: any) => (
           <EventCard key={event._id} event={event} />
         ))}
