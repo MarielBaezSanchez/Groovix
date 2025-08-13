eeb24e77cd7bee37b077a914ba5576f34daf37c2
MarielBaezSanchez <marielbaezsanchez991@gmail.com>
2025-08-12 19:30:34 -0600

    filtros de eventos

--- client/src/pages/private/home/index.tsx
+++ client/src/pages/private/home/index.tsx
@@ -1,42 +1,53 @@
 import { useEffect, useState } from "react";
-import usersGlobalStore, { type UsersStoreType } from "../../../store/users-store";
 import { message } from "antd";
 import { getEvents } from "../../../api-services/events-service";
-import type { EventType } from "../../../interfaces";
 import EventCard from "./common/event-card";
 import Filters from "./common/filters";
-
+import Spinner from "../../../components/spinner";
+import type { EventType } from "../../../interfaces";
+import type { UsersStoreType } from "../../../store/users-store";
+import usersGlobalStore from "../../../store/users-store";
 
 function Homepage() {
   const [events, setEvents] = useState<EventType[]>([]);
   const [filters, setFilters] = useState({
-    searchText:"",
+    searchText: "",
     date: "",
   });
   const [loading, setLoading] = useState(false);
   const { currentUser } = usersGlobalStore() as UsersStoreType;
 
-
-  const getData = async () => {
+  const getData = async (filtersObj: any) => {
     try {
       setLoading(true);
-      const response = await getEvents();
+      const response = await getEvents(filtersObj);
       setEvents(response.data);
     } catch (error) {
-      message.error("Falla al obtener los eventos");
+      message.error("Fallo al obtener eventos");
     } finally {
       setLoading(false);
     }
   };
 
   useEffect(() => {
-    getData();
+    getData({ searchText: "", date: "" });
   }, []);
-  
+
+  if (loading) {
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <Spinner />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <p>Welcome, {currentUser?.name}!</p>
-      <Filters filters={filters} setFilters={setFilters} onFilter={getData}/>
+      <p className="text-gray-600 text-xl font-bold">
+        Welcome, {currentUser?.name}!!!
+      </p>
+
+      <Filters filters={filters} setFilters={setFilters} onFilter={getData} />
 
       <div className="flex flex-col gap-7 mt-7">
         {events.map((event: any) => (
@@ -44,7 +55,7 @@ function Homepage() {
         ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
